8ef03b70a457822c0c47dbf70fe7b23042afb5ce
EdMacall <irvedwmac@gmail.com>
2017-05-06 13:03:02 -0400

    pushing all my images

---
+++ midway_v.0.00e/scripts/island.js
@@ -0,0 +1,22 @@
+// Simple carrier class
+
+function SimpleShip()
+{
+  this.name = "";
+  this.victoryPointValue = 0;
+  this.surfaceCombatFactor = 0;
+  this.screeningValue = 0;
+  this.damageCapacity = 0;
+  this.hitsTaken = 0;
+  this.x = posX;
+  this.y = posY;
+  this.image1 = "";
+  this.image2 = "";
+  this.side = 0;
+  this.initDiveBomber = 0;
+  this.initTorpedoBomber = 0;
+  this.initFighter = 0;
+  this.currentDiveBomber = 0;
+  this.currentTorpedoBomber = 0
+  this.currentFighter = 0;
+}
---
+++ scripts/AircraftShipCRT.js
@@ -0,0 +1,17 @@
+AircraftShipCRT = 
+{
+  {"1-3", "Miss    Lose 3 Sqd", "Miss    Lose 3 Sqd", "Miss    Lose 3 Sqd",
+          "Miss    Lose 4 Sqd", "Miss    Lose 4 Sqd", "1 Hit   Lose 4 Sqd"},
+  {"1-2", "Miss    Lose 2 Sqd", "Miss    Lose 2 Sqd", "Miss    Lose 3 Sqd",
+          "1 Hit   Lose 3 Sqd", "1 Hit   Lose 3 Sqd", "1 Hit   Lose 4 Sqd"},
+  {"1-1", "1 Hit   Lose 2 Sqd", "1 Hit   Lose 2 Sqd", "1 Hit   Lose 3 Sqd",
+          "1 Hit   Lose 4 Sqd", "2 Hits  Lose 4 Sqd", "2 Hits  Lose 3 Sqd"},
+  {"2-1", "1 Hit   Lose 2 Sqd", "1 Hit   Lose 3 Sqd", "1 Hit   Lose 3 Sqd",
+          "2 Hits  Lose 4 Sqd", "2 Hits  Lose 4 Sqd", "3 Hits  Lose 3 Sqd"},
+  {"3-1", "2 Hits  Lose 3 Sqd", "2 Hits  Lose 3 Sqd", "3 Hits  Lose 3 Sqd",
+          "3 Hits  Lose 2 Sqd", "4 Hits  Lose 2 Sqd", "4 Hits  Lose 2 Sqd"},
+  {"4-1", "3 Hits  Lose 3 Sqd", "3 Hits  Lose 2 Sqd", "4 Hits  Lose 2 Sqd",
+          "5 Hits  Lose 2 Sqd", "6 Hits  Lose 2 Sqd", "6 Hits  Lose 1 Sqd"},
+  {"5-1", "10 Hits Lose 0 Sqd", "10 Hits Lose 0 Sqd", "10 Hits Lose 0 Sqd",
+          "10 Hits Lose 0 Sqd", "10 Hits Lose 0 Sqd", "10 Hits Lose 0 Sqd"}
+};
